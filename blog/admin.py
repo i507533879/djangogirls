d3093f1ad4ace57036eae7fdbd7d6ca90ae5b21a
xia <1326454532@qq.com>
2024-08-08 20:33:48 +0800

    add create new project which is a web blog

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+'''
+Description: 
+Author: xwx
+Date: 2024-08-08 18:03:17
+LastEditTime: 2024-08-08 18:23:12
+LastEditors: xwx
+'''
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
